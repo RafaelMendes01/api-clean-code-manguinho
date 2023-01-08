c7465f18821218f3ea12ff19efe8d99b9a9f3c20
rafael mendes <rafael.mendes@tallos.com.br>
2023-01-08 16:35:36 -0300

    refactor: simplify signup method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,16 +3,11 @@ import { MissingParamError } from '../errors/missing-params-error'
 import { BadRequest } from '../helpers/http-helpers'
 
 export class SignUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return BadRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return BadRequest(new MissingParamError('email'))
-    } else {
-      return {
-        statusCode: 200,
-        body: new Error('all data is correctly')
+  handle (httpRequest: HttpRequest): HttpResponse | any {
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return BadRequest(new MissingParamError(field))
       }
     }
   }
